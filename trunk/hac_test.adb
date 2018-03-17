5b535a8c335c6725dffa9a35792ad2d0bc2c3f5a
gdemont <gdemont@348a34a6-7c62-431a-ae53-97a3626fe06e>
2018-03-17 21:23:05 +0000

    hac_test ok with hac_data spec.

--- trunk/hac_test.adb
+++ trunk/hac_test.adb
@@ -16,7 +16,7 @@ procedure HAC_Test is
     Put_Line ("*******[HAC]*******   Compiling from file: " & name);
     Open (f, In_File, name);
     HAC.Data.Line_Count:= 0;
-    HAC.Data.c_Set_Stream (HAC.Data.Stream_Access(Stream(f)));
+    HAC.Data.c_Set_Stream (HAC.Data.Stream_Access(Stream(f)), name);
     HAC.Compiler.Compile;
     Close (f);
     Put_Line ("*******[HAC]*******   Compilation finished.");
--- trunk/sma_exm/hello.adb
+++ trunk/sma_exm/hello.adb
@@ -1,6 +1,11 @@
 with HAC_Pack; use HAC_Pack;
+
 procedure Hello is
 begin
   Put("Hello");
+  Put_Line(" world!");
+  for i in 1 .. 10 loop
+    Put("Spaceman is speaking..."); Put(i); New_Line;
+  end loop;
 end;
 
