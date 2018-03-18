23ba6fba02abfbe4b51f3bd55e5a88a468064ad2
gdemont <gdemont@348a34a6-7c62-431a-ae53-97a3626fe06e>
2018-03-18 06:45:54 +0000

    More example source brush-up

--- sma_exm/merge_sort.adb
+++ sma_exm/merge_sort.adb
@@ -2,9 +2,6 @@ with HAC_Pack; use HAC_Pack;
 
 procedure Merge_Sort is
 
-  -- STIME : Float;
-  -- CH    : Character;
-
   procedure Merge is
 
    -- SUBTYPE VECTOR IS STRING(1..26); -- !! Hangs HAC
@@ -18,34 +15,33 @@ procedure Merge_Sort is
    left, top_left   : Integer;
    right, top_right : Integer;
 
-   c2        : String (1..3);
+   c2 : String (1..3);
 
   begin
     v := "ZYXWVUTSRQPONMLKJIHGFEDCBA";
     c2:= " rl";
     max := 26;
-    Put_Line ("Merge_Sort.");
+    Put_Line ("Merge Sort");
     New_Line;
-    Put_Line ("Unsorted string:");
+    Put_Line ("String at start:");
     Put_Line ("----------------");
     for k in 1..26 loop
       Put(v(k));
     end loop;
     New_Line;
     New_Line;
-    -- delay STIME - CLOCK;
+    --
     cur_length := 1;
-    while cur_length < max loop -- NEW PHASE
+    while cur_length < max loop  --  New phase
       temp_array := v;
       for k in 1..26 loop
         Put(temp_array(k));
         Put(c2(1));
       end loop;
       New_Line;
-
       left := 1;
       m := 1;
-      while left <= max loop -- FIND PAIR OF SUBARRAYS
+      while left <= max loop  --  Find pair of subarrays
         right := left + cur_length;
         top_left := right;
         if top_left > max then
@@ -55,8 +51,8 @@ procedure Merge_Sort is
         if top_right > max then
           top_right := max + 1;
         end if;
-        -- MERGE SUBARRAYS
-        -- GO UNTIL ONE SUBARRAY RUNS OUT
+        --  Merge subarrays
+        --  Go until one subarray runs out
         while (left < top_left) and (right < top_right) loop
           if temp_array(left) <= temp_array(right) then
             v(m)  := temp_array(left);
@@ -71,7 +67,7 @@ procedure Merge_Sort is
           end if;
           m := m + 1;
         end loop;
-        -- NOW "COPY TAIL" OF WHICHEVER SUBARRAY REMAINS
+        -- Now "copy tail" of whichever subarray remains
         while left < top_left loop
           v(m) := temp_array(left);
           Put(v(m));
@@ -89,11 +85,11 @@ procedure Merge_Sort is
         left := top_right;
       end loop;
       New_Line;
-      -- NOW DOUBLE SIZE OF SUBARRAYS AND GO BACK FOR NEXT PHASE
+      --  Now double size of subarrays and go back for next phase
       cur_length := cur_length * 2;
     end loop;
     New_Line;
-    Put_Line("Result of Merge_Sort:");
+    Put_Line("Result of Merge Sort:");
     Put_Line("---------------------");
     for k in 1..26 loop
       Put(v(k));
@@ -102,10 +98,5 @@ procedure Merge_Sort is
   end Merge;
 
 begin
-  -- STIME := CLOCK + 10.0;
-
   Merge;
-
-  -- PUT("PRESS ENTER TO RETURN TO THE EDITOR...");
-  -- GET_LINE(CH);
 end Merge_Sort;
--- sma_exm/shell_sort.adb
+++ sma_exm/shell_sort.adb
@@ -10,70 +10,71 @@ procedure Shell_Sort is
     step_size : array(1..4) of Integer;
     stop : Boolean;
     temp : Character;
-   begin
-     b := "ZYXWVUTSRQPONMLKJIHGFEDCBA";
-     Put_Line ("Shell_Sort.");
-     Put_Line ("Unsorted string:");
-     Put_Line ("----------------");
-     for k in 1..26 loop
-       Put(b(k));
-     end loop;
-     New_Line;
-     New_Line;
-     -- Put_Line(B);
-     -- 'steps' contains decreasing increments for each
-     -- pass. The last pass has increment 1.
-     step_size(4) := 1;
-     for pass in reverse 1..3 loop
-       step_size(pass):= 2*step_size(pass+1);
-     end loop;
-     for pass in 1..4 loop
-       -- cursorat(14,45);
-       Put("Pass: "); Put(pass);
-       New_Line;
-       step := step_size(pass);
-       Put("Step: "); Put(step);
-       New_Line;
-         -- Do a straight insertion sort with 'step' as
-         -- an increment instead of 1.
-       i:= step + 1;
-       while i <= 26 loop
-         --cursorat(15,45);
-         New_Line;
-         Put("I:    "); Put(i); New_Line;
-         temp := b(i);
-         j:= i;
-         stop:= False;
-         while (j > step) and not stop loop
-           j := j - step;
-           -- cursorat(16,45); put("J:    "); put(J);
-           if b(j) > temp then
-             b(j+step):= b(j);
-             -- cursorat(15,14+j);
-             Put(temp);
-           else
-             b(j+step):= temp;
-             stop:= True;
-           end if;
-           CursorAt(15,14+(j+step)); Put(b(j+step));
-         end loop;
-         if not stop then
-           b(1):= temp;
-           CursorAt(15,14+0);
-           Put(temp);
-         end if;
-         i := i + step;
-       end loop;
-       New_Line;
-     end loop; -- for pass in 1..npass
-     New_Line;
-     Put_Line("Result of Shell_Sort:");
-     Put_Line("---------------------");
-     for k in 1..26 loop
-       Put(b(k));
-     end loop;
-     New_Line;
-   end Shell;
+  begin
+    b := "ZYXWVUTSRQPONMLKJIHGFEDCBA";
+    Put_Line ("Shell Sort");
+    New_Line;
+    Put_Line ("String at start:");
+    Put_Line ("----------------");
+    for k in 1..26 loop
+      Put(b(k));
+    end loop;
+    New_Line;
+    New_Line;
+    -- Put_Line(B);
+    -- 'steps' contains decreasing increments for each
+    -- pass. The last pass has increment 1.
+    step_size(4) := 1;
+    for pass in reverse 1..3 loop
+      step_size(pass):= 2*step_size(pass+1);
+    end loop;
+    for pass in 1..4 loop
+      -- cursorat(14,45);
+      Put("Pass: "); Put(pass);
+      New_Line;
+      step := step_size(pass);
+      Put("Step: "); Put(step);
+      New_Line;
+        -- Do a straight insertion sort with 'step' as
+        -- an increment instead of 1.
+      i:= step + 1;
+      while i <= 26 loop
+        --cursorat(15,45);
+        New_Line;
+        Put("I:    "); Put(i); New_Line;
+        temp := b(i);
+        j:= i;
+        stop:= False;
+        while (j > step) and not stop loop
+          j := j - step;
+          -- cursorat(16,45); put("J:    "); put(J);
+          if b(j) > temp then
+            b(j+step):= b(j);
+            -- cursorat(15,14+j);
+            Put(temp);
+          else
+            b(j+step):= temp;
+            stop:= True;
+          end if;
+          CursorAt(15,14+(j+step)); Put(b(j+step));
+        end loop;
+        if not stop then
+          b(1):= temp;
+          CursorAt(15,14+0);
+          Put(temp);
+        end if;
+        i := i + step;
+      end loop;
+      New_Line;
+    end loop; -- for pass in 1..npass
+    New_Line;
+    Put_Line("Result of Shell Sort:");
+    Put_Line("---------------------");
+    for k in 1..26 loop
+      Put(b(k));
+    end loop;
+    New_Line;
+  end Shell;
 
 begin
   Shell;
