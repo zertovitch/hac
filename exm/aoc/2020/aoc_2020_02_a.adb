ada13d3dd4dfdd5b0e60dfb812555135434491d5
unknown <gdemont@hotmail.com>
2020-12-03 06:57:59 +0100

    (examples) Simplified parsing in Advent of Code 2020, Day 2, solutions

--- exm/aoc/2020/aoc_2020_02_a.adb
+++ exm/aoc/2020/aoc_2020_02_a.adb
@@ -17,36 +17,24 @@ with HAC_Pack;  use HAC_Pack;
 procedure AoC_2020_02_a is
   s : VString;
   f : File_Type;
-  c : Character;
+  c, sep : Character;
   i, n, valid, min, max : Integer;
 begin
   Open (f, "aoc_2020_02.txt");
   valid := 0;
   while not End_Of_File (f) loop
+    Get (f, min);
+    Get (f, sep);
+    Get (f, max);
+    Get (f, sep);
+    Get (f, c);
+    Get (f, sep);
+    Get (f, sep);
     Get_Line (f, s);
-    min := 0;
-    max := 0;
     i := 1;
-    loop
-      c := Element (s, i);
-      exit when c = '-';
-      min := min * 10 + (Ord (c) - Ord ('0'));
-      i := i + 1;
-    end loop;
-    i := i + 1;
-    loop
-      c := Element (s, i);
-      exit when c = ' ';
-      max := max * 10 + (Ord (c) - Ord ('0'));
-      i := i + 1;
-    end loop;
-    i := i + 1;
-    c := Element (s, i);
-    i := i + 3;
     n := 0;
-    loop
+    while i <= Length (s) loop
       if c = Element (s, i) then n := n + 1; end if;
-      exit when i = Length (s);
       i := i + 1;
     end loop;
     if (n >= min) and (n <= max) then
--- exm/aoc/2020/aoc_2020_02_b.adb
+++ exm/aoc/2020/aoc_2020_02_b.adb
@@ -15,39 +15,25 @@ with HAC_Pack;  use HAC_Pack;
 procedure AoC_2020_02_b is
   s : VString;
   f : File_Type;
-  c : Character;
-  i, j, n, valid, pos1, pos2 : Integer;
+  c, sep : Character;
+  i, n, valid, pos1, pos2 : Integer;
 begin
   Open (f, "aoc_2020_02.txt");
   valid := 0;
   while not End_Of_File (f) loop
+    Get (f, pos1);
+    Get (f, sep);
+    Get (f, pos2);
+    Get (f, sep);
+    Get (f, c);
+    Get (f, sep);
+    Get (f, sep);
     Get_Line (f, s);
-    pos1 := 0;
-    pos2 := 0;
     i := 1;
-    loop
-      c := Element (s, i);
-      exit when c = '-';
-      pos1 := pos1 * 10 + (Ord (c) -  Ord ('0'));
-      i := i + 1;
-    end loop;
-    i := i + 1;
-    loop
-      c := Element (s, i);
-      exit when c = ' ';
-      pos2 := pos2 * 10 + (Ord (c) -  Ord ('0'));
-      i := i + 1;
-    end loop;
-    i := i + 1;
-    c := Element (s, i);
-    i := i + 3;
     n := 0;
-    j := 1;
-    loop
-      if (c = Element (s, i)) and ((j = pos1) or (j = pos2)) then n := n + 1; end if;
-      exit when i = Length (s);
+    while i <= Length (s) loop
+      if (c = Element (s, i)) and ((i = pos1) or (i = pos2)) then n := n + 1; end if;
       i := i + 1;
-      j := j + 1;
     end loop;
     if n = 1 then
       valid := valid + 1;
