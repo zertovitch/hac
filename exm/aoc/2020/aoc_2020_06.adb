6bbb80ad9a0db1f2c5385ea6ced463ab43bd4adf
unknown <gdemont@hotmail.com>
2020-12-06 14:09:21 +0100

    (examples) Advent of Code 2020, Day 6, solution: HAC version accepted by a full Ada compiler

--- exm/aoc/2020/aoc_2020_06.adb
+++ exm/aoc/2020/aoc_2020_06.adb
@@ -10,11 +10,11 @@
 --
 --  HAC 0.08 "nice to have"'s detected in this exercise:
 --
---    *     ` subtype ... range ... ` like: `subtype Answer_Range is Character range 'a' .. 'z';`
+--    *     ` subtype ... range ... ` like: `subtype Answer_Range is Character range Answer_Range;`
 --    *     ` clear := (others => False); `
 --    *     ` rg := rg and r ` for arrays of Boolean (i.e., sets)
---    *     ` aaa : constant Character := 'a';` compiled as a "Pascal" constant (like a number)
---                                              and not as a read-only variable
+--    *     ` aaa : constant Character := 'a';`
+--                       HAC should detect an expression as a static (compile-time-known) value
 --    *     in HAC_Pack: Index (s, c) where c is a Character
 --
 with HAC_Pack;  use HAC_Pack;
@@ -22,16 +22,15 @@ with HAC_Pack;  use HAC_Pack;
 procedure AoC_2020_06 is
   total : Integer;
   new_group : Boolean;
-  --  aaa : constant Character := 'a';
-  --  ^ Avoid ambiguity of Wide_*Character when compiled with full Ada.
-  --    Replace all `'a'` below by `aaa` for compiling with a full Ada compiler.
-  type Yes_Answer is array ('a' .. 'z') of Boolean;
+  subtype Answer_Range is Character;
+  --  !!  subtype Answer_Range is Character range 'a' .. 'z' (the program works without it because of well-formed data)
+  type Yes_Answer is array (Answer_Range) of Boolean;
   r, rg : Yes_Answer;
   --
   procedure Collect_Group_Total is
     g : Natural := 0;
   begin
-    for c in 'a' .. 'z' loop if rg (c) then g := g + 1; end if; end loop;
+    for c in Answer_Range loop if rg (c) then g := g + 1; end if; end loop;
     total := total + g;
     new_group := True;
   end Collect_Group_Total;
@@ -48,16 +47,16 @@ begin
       if s = "" then
         Collect_Group_Total;
       else
-        for c in 'a' .. 'z' loop
+        for c in Answer_Range loop
           r (c) := Index (s, +c) > 0;
         end loop;
         if new_group then
           rg := r;
           new_group := False;
         elsif part = 1 then
-          for c in 'a' .. 'z' loop rg (c) := rg (c) or r (c); end loop;
+          for c in Answer_Range loop rg (c) := rg (c) or r (c); end loop;
         else
-          for c in 'a' .. 'z' loop rg (c) := rg (c) and r (c); end loop;
+          for c in Answer_Range loop rg (c) := rg (c) and r (c); end loop;
         end if;
       end if;
     end loop;
