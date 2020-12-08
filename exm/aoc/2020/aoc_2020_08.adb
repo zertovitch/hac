ba244e5ec7cc43bdff28cf31b53ce9c2398c387f
unknown <gdemont@hotmail.com>
2020-12-08 07:06:00 +0100

    (examples) Advent of Code 2020, Day 8, solution: made nicer

--- exm/aoc/2020/aoc_2020_08.adb
+++ exm/aoc/2020/aoc_2020_08.adb
@@ -4,24 +4,27 @@
 --
 --  https://adventofcode.com/2020/day/8
 --
---
 --  HAC 0.081 version.
 --
 --  HAC 0.081 "nice to have"'s detected in this exercise:
 --
 --    *     I/O with enums, at least 'Image
+--    *     `  seen := (others => False);  `
+--    *     Get with String parameter, as in `  Get (f, asm);  `
+--    *     String-to-literal comparison
+--             as in `  if asm = "acc" then i := acc; end if;  `
 --
 with HAC_Pack;  use HAC_Pack;
 
 procedure AoC_2020_08 is
+  subtype Machine_Code_Range is Positive range 1 .. 1000;
   type Instr is (acc, jmp, nop);
-  max : constant := 1000;
-  code : array (1 .. max) of Instr;
-  oper : array (1 .. max) of Integer;
+  code : array (Machine_Code_Range) of Instr;
+  oper : array (Machine_Code_Range) of Integer;
   last : Natural := 0;
   --
   procedure Run (verbose : Boolean) is
-    seen : array (1 .. max) of Boolean;
+    seen : array (Machine_Code_Range) of Boolean;
     a : Integer := 0;
     c : Integer := 1;
     ok : Boolean;
@@ -33,7 +36,7 @@ procedure AoC_2020_08 is
     while c <= last loop
       seen (c) := True;
       case code (c) is
-        when nop =>  c := c + 1;
+        when nop => c := c + 1;
         when acc => a := a + oper (c); c := c + 1;
         when jmp => c := c + oper (c);
       end case;
@@ -41,8 +44,10 @@ procedure AoC_2020_08 is
     end loop;
     ok := c > last;
     if verbose or ok then
-       Put (+"Acc = " & a);
-      if ok then Put_Line (" OK!"); else Put_Line (" infinite loop"); end if;
+      Put (+"Accumulator = " & a & ";  ");
+      if ok then Put_Line ("correct exit!");
+            else Put_Line ("infinite loop.");
+      end if;
     end if;
   end Run;
   --
@@ -54,19 +59,20 @@ procedure AoC_2020_08 is
       when jmp => code (c) := nop;
     end case;
   end Swap;
-  c1, c2, c3 : Character;
+  --
+  asm : String (1 .. 3);
   i : Instr;
   v : Integer;
   f : File_Type;
 begin
   Open (f, "aoc_2020_08.txt");
   while not End_Of_File (f) loop
-    Get (f, c1);
-    Get (f, c2);
-    Get (f, c3);
-    if +"" & c1 & c2 & c3 = "acc" then i := acc; end if;
-    if +"" & c1 & c2 & c3 = "jmp" then i := jmp; end if;
-    if +"" & c1 & c2 & c3 = "nop" then i := nop; end if;
+    Get (f, asm (1));
+    Get (f, asm (2));
+    Get (f, asm (3));
+    if +asm = "acc" then i := acc; end if;
+    if +asm = "jmp" then i := jmp; end if;
+    if +asm = "nop" then i := nop; end if;
     Get (f, v);
     last := last + 1;
     code (last) := i;
@@ -77,7 +83,7 @@ begin
   --
   Run (True);
   --
-  --  Fix the code:
+  --  Try fixing the machine code:
   --
   for c in 1 .. last loop
     Swap (c);
