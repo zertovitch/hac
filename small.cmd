87be83107122944d79f0b76ea34c1bbd42710a68
zertovitch <gdemont@hotmail.com>
2023-07-16 23:46:58 +0200

    (GNAT project file) Added a Small_Unchecked build mode

--- small.cmd
+++ small.cmd
@@ -1,9 +1,23 @@
 @echo off
 echo Build...
-gprbuild -P hac           -XHAC_Build_Mode=Small -largs obj/hac_icon.rbj
-gprbuild -P demo/hac_demo -XHAC_Build_Mode=Small -largs obj/hac_icon.rbj
+del hac*.exe
+gprbuild -P hac hac       -XHAC_Build_Mode=Small_Unchecked
+copy /B hac.exe hac_unchecked.exe
+del hac.exe
+gprbuild -P hac           -XHAC_Build_Mode=Small           -largs obj/hac_icon.rbj
+del demo\*.exe
+gprbuild -P demo/hac_demo -XHAC_Build_Mode=Small           -largs obj/hac_icon.rbj
 
-for %%I in (hac.exe) do echo %%~zI
-echo Strip...
-strip -s hac.exe
-for %%I in (hac.exe) do echo %%~zI
+echo.
+for %%I in (hac*.exe) do echo %%~zI ... %%I
+for %%I in (demo\*.exe) do echo %%~zI ... %%I
+
+echo.
+echo Stripping...
+strip -s hac*.exe
+strip -s demo/*.exe
+
+echo Done.
+echo.
+for %%I in (hac*.exe) do echo %%~zI ... %%I
+for %%I in (demo\*.exe) do echo %%~zI ... %%I
