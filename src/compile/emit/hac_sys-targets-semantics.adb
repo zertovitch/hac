347ddec867d9c1f4b4ff9a255ab57ff948221a98
zertovitch <gdemont@hotmail.com>
2023-09-01 06:05:39 +0200

    (targets) Progress on Semantics target

--- src/compile/emit/hac_sys-targets-semantics.adb
+++ src/compile/emit/hac_sys-targets-semantics.adb
@@ -1,21 +1,39 @@
 package body HAC_Sys.Targets.Semantics is
 
-   ----------------------
-   -- Mark_Declaration --
-   ----------------------
+  overriding procedure Initialize_Code_Emission (m : in out Machine) is
+    use HAT;
+  begin
+    m.busy := True;
+    m.started := Clock;
+  end Initialize_Code_Emission;
 
-   overriding procedure Mark_Declaration (m : Machine) is
-   begin
-     null;
-   end Mark_Declaration;
+  overriding procedure Finalize_Code_Emission
+    (m       : in out Machine;
+     strings :        String)
+  is
+    use HAT;
+  begin
+    m.finished := Clock;
+    m.total_time := m.finished - m.started;
+    m.busy := False;
+  end Finalize_Code_Emission;
 
-   --------------------
-   -- Mark_Reference --
-   --------------------
+  ----------------------
+  -- Mark_Declaration --
+  ----------------------
 
-   overriding procedure Mark_Reference (m : Machine) is
-   begin
-     null;
-   end Mark_Reference;
+  overriding procedure Mark_Declaration (m : Machine) is
+  begin
+    null;
+  end Mark_Declaration;
+
+  --------------------
+  -- Mark_Reference --
+  --------------------
+
+  overriding procedure Mark_Reference (m : Machine) is
+  begin
+    null;
+  end Mark_Reference;
 
 end HAC_Sys.Targets.Semantics;
--- src/compile/emit/hac_sys-targets-semantics.ads
+++ src/compile/emit/hac_sys-targets-semantics.ads
@@ -12,30 +12,60 @@
 --  serves semantics analysis purposes, like cross-references
 --  for an editor with auto-complete, contextual menus and tool tips.
 
+with Ada.Containers.Hashed_Maps,
+     Ada.Strings.Unbounded.Hash;
+
 with HAC_Sys.Co_Defs;
+
+with HAT;
 
 package HAC_Sys.Targets.Semantics is
 
+  --  Reference map
+  --  =============
+  --
+  --  Key                  Value
+  --  ===                  =====
+  --  [file_name i j]      index in the Id Table
+
+  package Reference_Mapping is new Ada.Containers.Hashed_Maps
+    (Key_Type        => HAT.VString,  --  [file_name i j]
+     Element_Type    => Positive,     --  Index in the Id Table
+     Hash            => Ada.Strings.Unbounded.Hash,
+     Equivalent_Keys => Ada.Strings.Unbounded."=");
+
   --  Declarations map
   --  ================
-  --  
+  --
   --  Key                  Value
   --  ===                  =====
   --  index in Id Table    file_name, i, j of declaration
-  --  
-  --  
-  --  Reference map
-  --  =============
-  --  
-  --  Key                  Value                  Note
-  --  ===                  =====                  ====
-  --  [file_name i j]      index in Id Table      compare identifiers first
-  --  
+
+  type Declaration_Point is record
+    file_name : HAT.VString;
+    i, j      : Positive;
+  end record;
+
+  type Declaration_Point_Array is
+    array (Co_Defs.Identifier_Table_Type'Range) of Declaration_Point;
+
+  --  Possible sanity checks for an usage within an editor:
+  --    - compare identifiers between the one at [file_name i j] and
+  --      the one in the identifier table (weak check)
+  --    - ensure the analysis was completed after the latest
+  --      modification in the editor (keystroke, cut, paste, ...)
+  --
 
   type Machine is limited new Targets.Machine with record
     CD : Co_Defs.Compiler_Data_Access;
     --    ^ We keep this field public for convenience (it is
     --      needed in method Set_Target in Co_Defs).
+    ref_map    : Reference_Mapping.Map;
+    decl_map   : Declaration_Point_Array;
+    busy       : Boolean  := False with Volatile;
+    started    : HAT.Time;
+    finished   : HAT.Time;
+    total_time : Duration := 0.0;
   end record;
 
   --------------------
@@ -46,6 +76,15 @@ package HAC_Sys.Targets.Semantics is
   overriding function CPU (m : Machine) return String is ("No CPU");
   overriding function OS (m : Machine) return String is ("Any");
   overriding function Null_Terminated_String_Literals (m : Machine) return Boolean is (False);
+
+  ---------------------------------------
+  --  Initialize & Finalize Semantics  --
+  ---------------------------------------
+
+  overriding procedure Initialize_Code_Emission (m : in out Machine);
+  overriding procedure Finalize_Code_Emission
+    (m       : in out Machine;
+     strings :        String);
 
   ----------------------------
   --  Machine Instructions  --
