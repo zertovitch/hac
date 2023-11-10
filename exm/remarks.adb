9fb8a962feaa05ba0f287a486d6840131a70641f
unknown <gdemont@hotmail.com>
2023-11-10 22:29:19 +0100

    (examples) Improved comments on the `Remarks` example

--- exm/remarks.adb
+++ exm/remarks.adb
@@ -1,24 +1,25 @@
---  Remarks emitted by the compiler.
+--  Remarks (warnings and notes) emitted by the HAC compiler.
 --
 --  Command sample:
---  hac -rkruv remarks.adb
+--
+--      hac        -rkruv remarks.adb
+--      gcc -c -gnatwkruv remarks.adb
+
+with Interfaces;
 
 procedure Remarks is
-  --  Unused:
-  a  : Integer;
-  type B is (x, y);
-  procedure C is null;
-  d : Integer := 123;
-  --  Unused (never read):
-  e : Integer;
-  f : Integer := 123;
-  --  Could be constant:
-  g : Integer := 123;
-  --  Read but never written:
-  h : Integer;
-  --  Should issue no insult:
-  type A0 is (x0, y0);
-  b0 : A0 := x0;
+  use Interfaces;
+  use Interfaces;       --  Note:     Redundant [-rr]
+  a  : Integer;         --  Note:     Unused [-ru]
+  type B is (x, y);     --  Note:     Unused [-ru]
+  procedure C is null;  --  Note:     Unused [-ru]
+  d : Integer := 123;   --  Note:     Unused [-ru]
+  e : Integer;          --  Note:     Never read [-ru]
+  f : Integer := 123;   --  Note:     Never read [-ru]
+  g : Integer := 123;   --  Note:     Could be constant [-rk]
+  h : Integer;          --  Warning:  Read but never written [-rv]
+  type A0 is (x0, y0);  --  Note:     "y0" is unused [-ru] 
+  b0 : A0 := x0;        --  Note:     "b0" is unused [-ru]
 begin
   e := 0;
   f := g + h;
