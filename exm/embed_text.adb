ad8f91f4d2d252be6af04aa08359061574d1a3c3
unknown <gdemont@hotmail.com>
2025-06-07 14:03:45 +0200

    (examples) embed_text.adb: output already split into 3 Ada files; switch to produce sources using Ada.* packages

--- exm/embed_text.adb
+++ exm/embed_text.adb
@@ -11,18 +11,38 @@ procedure Embed_Text is
   f1, f2 : File_Type;
   lines : Natural := 0;
   c : Character;
+  type Output_Mode is (hac, full_ada);
+  output : Output_Mode := hac;
 begin
   if Argument_Count < 2 then
-    Put_Line ("Usage: hac embed_text.adb [text_file] [ada_package_name]");
-    Put_Line ("Produces: embedded.ada (to be split in 3, manually or by gnatchop)");
+    Put_Line ("Usage: hac embed_text.adb text_file ada_package_name [-h] [-f]");
+    Put_Line ("   or:     embed_text     text_file ada_package_name [-h] [-f]");
+    New_Line;
+    Put_Line ("Produces: ada_package_name.ads, ada_package_name.adb, demo_ada_package_name.adb");
+    New_Line;
+    Put_Line ("Options: -h : produce sources for HAC, with HAT package (default)");
+    Put_Line ("         -f : produce sources for ""full Ada"", with Ada.* packages");
     return;
   end if;
   Open (f1, Argument (1));
-  Create (f2, "embedded.ada");
   pkg := Argument (2);
+  for i in 3 .. Argument_Count loop
+    if Argument (i) = "-h" then
+      output := hac;
+    elsif Argument (i) = "-f" then
+      output := full_ada;
+    end if;
+  end loop;
+  Create (f2, To_Lower (pkg) & ".adb");
   Put_Line (f2, "package body " & pkg & " is");
   Put_Line (f2, "  procedure Fill_Contents (text : out Embedded_Text) is");
-  Put_Line (f2, "    use HAT;");
+  case output is
+    when hac      =>
+      Put_Line (f2, "    use HAT;");
+    when full_ada => 
+      Put_Line (f2, "    use Ada.Strings.Unbounded;");
+      Put_Line (f2, "    function ""+"" (S : String) return Unbounded_String renames To_Unbounded_String;");
+  end case;
   Put_Line (f2, "  begin");
   while not End_Of_File (f1) loop
     lines := lines + 1;
@@ -41,18 +61,44 @@ begin
   Close (f1);
   Put_Line (f2,  "  end Fill_Contents;");
   Put_Line (f2,  "end " & pkg & ";");
-  Put_Line (f2,  "with HAT;");
+  Close (f2);
+  Create (f2, To_Lower (pkg) & ".ads");
+  case output is
+    when hac      =>
+      Put_Line (f2,  "with HAT;");
+    when full_ada =>
+      Put_Line (f2,  "with Ada.Strings.Unbounded;");
+  end case;
   Put_Line (f2,  "package " & pkg & " is");
-  Put_Line (f2, +"  type Embedded_Text is array (1 .. " & lines & ") of HAT.VString;");
+  Put (f2, +"  type Embedded_Text is array (1 .. " & lines & ") of ");
+  case output is
+    when hac      =>
+      Put_Line (f2,  "HAT.VString;");
+    when full_ada =>
+      Put_Line (f2,  "Ada.Strings.Unbounded.Unbounded_String;");
+  end case;
   Put_Line (f2,  "  procedure Fill_Contents (text : out Embedded_Text);");
   Put_Line (f2,  "end " & pkg & ";");
-  Put_Line (f2,  "with HAT, " & pkg & ';');
+  Close (f2);
+  Create (f2, "demo_" & To_Lower (pkg) & ".adb");
+  case output is
+    when hac      =>
+      Put_Line (f2,  "with HAT;");
+    when full_ada =>
+      Put_Line (f2,  "with Ada.Strings.Unbounded, Ada.Text_IO;");
+  end case;
+  Put_Line (f2,  "with " & pkg & ';');
   Put_Line (f2,  "procedure Demo_" & pkg & " is");
   Put_Line (f2,  "  e : " & pkg & ".Embedded_Text;");
   Put_Line (f2,  "begin");
   Put_Line (f2,  "  " & pkg & ".Fill_Contents (e);");
   Put_Line (f2,  "  for l in e'Range loop");
-  Put_Line (f2,  "    HAT.Put_Line (e (l));");
+  case output is
+    when hac      =>
+      Put_Line (f2,  "    HAT.Put_Line (e (l));");
+    when full_ada =>
+      Put_Line (f2,  "    Ada.Text_IO.Put_Line (Ada.Strings.Unbounded.To_String (e (l)));");
+  end case;
   Put_Line (f2,  "  end loop;");
   Put_Line (f2,  "end Demo_" & pkg & ';');
   Close (f2);
